3725e2128b18909b72d2206ee6fb124e199ce629
Tu Bui <buituantu@vnpt.vn>
2018-10-24 14:57:57 +0700

    - Implement line aiming mechanic - Import Vectrosity plugin

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,20 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InputHandler : MonoBehaviour,IPointerDownHandler {
+public class InputHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 
     public BallController ballController;
+    public RaycastShoooter rayShooter;
 
-    public void OnMouseDown()
+    private bool isAimMove = false;
+    private int InputLayer;
+    private LayerMask InputMask;
+
+    public void Start()
     {
-        Debug.Log("pressed");
+        InputLayer = LayerMask.NameToLayer("Input");
+        InputMask = 1 << InputLayer;
+    }
+
+    public void Update()
+    {
+        if (isAimMove)
+        {
+            Vector2 dir = ballController.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Debug.DrawRay(ballController.transform.position, dir * 100, Color.green);
+
+            rayShooter.DrawRay(ballController.transform.position, dir);
+        }
     }
 
     public void OnPointerDown(PointerEventData data)
     {
-        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        // Raycast Style
+        //RaycastHit2D[] hits = null;
 
-        ballController.HandlingMousePressed(mousePos);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //hits = Physics2D.RaycastAll(ray.origin, ray.direction, 100f);
+
+        //var hitCount = Physics2D.RaycastNonAlloc(ray.origin, ray.direction, hits, 100f);
+        //if (hits.Length > 0)
+        //{            
+        //    foreach (var hitObject in hits)
+        //    {
+        //        if (hitObject.transform.gameObject.CompareTag("Ball"))
+        //        {
+        //            isAimMove = true;
+        //        }
+        //    }
+        //}
+
+        // Overlap point Style
+        var collider = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition), InputMask);
+        if (collider != null && collider.gameObject.CompareTag("FireStart"))
+        {
+            isAimMove = true;
+            rayShooter.EnableRay();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        isAimMove = false;
+        rayShooter.DisableRay();
     }
 }
--- Assets/Scripts/RaycastShoooter.cs
+++ Assets/Scripts/RaycastShoooter.cs
@@ -2,28 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Vectrosity;
 
-public class RaycastShoooter : MonoBehaviour, IPointerDownHandler
+public class RaycastShoooter : MonoBehaviour
 {
-    public GameObject ball;
-    public LineRenderer line;
-	// Use this for initialization
-	void Start () {
-        line = this.GetComponent<LineRenderer>();
+    public int RayCount;
+    public GameObject Ball;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        var dir = ball.transform.up;
-        Debug.DrawRay(ball.transform.position, dir * 10, Color.green);
-        line.SetPosition(0, ball.transform.position);
-        line.SetPosition(1, dir * 10);
-	}
+    public Color lineColor;
 
-    public void OnPointerDown(PointerEventData data)
-    {
-        Debug.Log("clicked");
+    private float RayLength = 100f;
+    private LineRenderer Line;
+    private int WallLayer;
+    private LayerMask WallMask;
+
+    private VectorLine aimLine;
+
+    void Start() {
+        Line = this.GetComponent<LineRenderer>();
+        WallLayer = LayerMask.NameToLayer("Block");
+        WallMask = 1 << WallLayer;
+
+        //aimLine = VectorLine.SetLine(lineColor, new Vector2(0f, 0f), new Vector2(0f, 0f));
+        aimLine = new VectorLine("aimLine", new List<Vector2>(), 1f);
     }
 
+    void Update() {
+        //var dir = Ball.transform.up;
+        //Debug.DrawRay(Ball.transform.position, dir * 10, Color.green);
+        //Line.SetPosition(0, Ball.transform.position);
+        //Line.SetPosition(1, dir * 10);
+    }
+
+    public void DrawRay(Vector2 rayPos, Vector2 rayDir)
+    {
+        List<Vector2> ListPos = new List<Vector2>();
+
+        // Raycast way
+
+        //Debug.DrawRay(rayPos, rayDir * 100, Color.green);
+        //ListPos.Add(rayPos);
+
+        //RaycastHit2D hit = Physics2D.Raycast(rayPos, rayDir, 100, WallMask);
+        ////var hits = Physics2D.RaycastAll(rayPos, rayDir, Mathf.Infinity, mask);
+        //if (hit.collider != null)
+        //{
+
+        //    ListPos.Add(hit.point);
+
+        //    //Debug.Log(hit.collider.gameObject.tag);
+        //    Vector2 reflect = Vector2.Reflect(hit.point - rayPos, hit.normal);
+        //    Debug.DrawRay(hit.point, reflect * 100, Color.blue);
+
+        //    var start = hit.point;
+
+        //    var hit2 = Physics2D.Raycast(hit.point, reflect, 100, WallMask);
+        //    if (hit2.collider != null)
+        //    {
+        //        ListPos.Add(hit2.point);
+
+        //        Debug.Log(hit2.point);
+        //        reflect = Vector2.Reflect(hit2.point - start, hit2.normal);
+        //        Debug.DrawRay(hit2.point, reflect * 100, Color.red);
+        //    }
+        //}
+
+        // RaycastAll way 
+        //for (int i = 0; i < RayCount; i++)
+        //{
+        //    ListPos.Add(rayPos);
+        //    Debug.DrawRay(rayPos, rayDir * RayLength, Color.green);
+        //    RaycastHit2D[] hits = Physics2D.RaycastAll(rayPos, rayDir, RayLength, WallMask);           
+
+        //    rayDir = Vector2.Reflect(hits[0].point - rayPos, hits[0].normal);
+        //    rayPos = hits[0].point;
+        //}
+        //ListPos.Add(rayPos);
+
+        //Line.positionCount = ListPos.Count;
+        //// Draw linerenderer
+        //for (int i = 0; i < ListPos.Count; i++)
+        //{
+        //    Line.SetPosition(i, ListPos[i]);
+        //}
+
+        // Using Vectrosity line
+        for (int i = 0; i < RayCount; i++)
+        {
+            ListPos.Add(rayPos);
+            Debug.DrawRay(rayPos, rayDir * RayLength, Color.green);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(rayPos, rayDir, RayLength, WallMask);
+
+            rayDir = Vector2.Reflect(hits[0].point - rayPos, hits[0].normal);
+            rayPos = hits[0].point;
+        }
+        ListPos.Add(rayPos);
+
+        for (int i = 0; i < ListPos.Count; i++)
+        {
+            ListPos[i] = Camera.main.WorldToScreenPoint(ListPos[i]);
+        }
+
+        aimLine = VectorLine.SetLine(lineColor, ListPos.ToArray());
+        //aimLine.points2.Add()
+        aimLine.Draw();
+    }
+
+    public void EnableRay()
+    {
+        Line.enabled = true;
+    }
+
+    public void DisableRay()
+    {
+        Line.enabled = false;
+    }
 }
