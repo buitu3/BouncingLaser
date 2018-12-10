4acfb9b78bf2a4e72baea3729ea2480996ed0ca3
buitu3 <buitu3@gmail.com>
2018-12-10 12:44:22 +0700

    - Cap nhat co che tinh lives va tu huy cho block

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public TextMeshProUGUI BlockCount;
+
+	// Use this for initialization
+	void Start () {
+        BlockCount.text = "Block Counts: " + "7";
+	}
+
+}
